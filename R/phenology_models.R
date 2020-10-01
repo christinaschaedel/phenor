dda5e861dca8f955ff2da420895dce7218274543
christinaschaedel <43353159+christinaschaedel@users.noreply.github.com>
2020-09-30 19:29:42 -0700

    Update phenology_models.R
    add CDDP model

--- R/phenology_models.R
+++ R/phenology_models.R
@@ -125,6 +125,37 @@ CDDs <- function(par, data){
   shape_model_output(data = data, doy = doy)
 }
 
+#' Chilling Degree Day with photoperiod(CDDP) adapted from PTT
+CDDP <- function(par, data){
+  # exit the routine as some parameters are missing
+  if (length(par) != 3){
+    stop("model parameter(s) out of range (too many, too few)")
+  }
+
+  # extract the parameter values from the
+  # par argument for readability
+  t0 <- round(par[1]) # int
+  T_base <- par[2]
+  F_crit <- par[3]
+
+  # create forcing/chilling rate vector
+  # forcing
+  Rf <- data$Tmini - T_base
+  Rf[Rf > 0] <- 0
+  Rf <- ((1 - data$Li) / 24) * Rf
+  Rf[1:t0,] <- 0
+ 
+  # DOY of budburst criterium
+  doy <- apply(Rf,2, function(xt){
+    data$doy[which(cumsum(xt) >= F_crit)[1]]
+  })
+
+  # set export format, either a rasterLayer
+  # or a vector
+  shape_model_output(data = data, doy = doy)
+}
+
+
 #' DormPhot model as defined in
 #' Caffarra, Donnelly and Chuine 2011 (Clim. Res.)
 #' parameter ranges are taken from Basler et al. 2016
